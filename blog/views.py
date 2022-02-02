0724d039d522c4e35805ab835fdf15b7297167a9
taranovev <tar_ev@mail.ru>
2022-02-02 23:18:09 +0300

    Reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def serialize_post_optimized(post):
         'published_at': post.published_at,
         'slug': post.slug,
         'tags': [
-            serialize_tag_optimized(tag) 
+            serialize_tag_optimized(tag)
             for tag in post.tags.all().annotate(posts_count=Count('posts'))
         ],
         'first_tag_title': post.tags.all()[0].title,
@@ -48,6 +48,7 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
+
 def serialize_tag_optimized(tag):
     return {
         'title': tag.title,
@@ -57,12 +58,14 @@ def serialize_tag_optimized(tag):
 
 def index(request):
 
-    
-    all_posts = Post.objects.prefetch_related('author').prefetch_related('tags')
+    all_posts = (
+        Post.objects.prefetch_related('author').prefetch_related('tags')
+    )
 
     most_popular_posts = (
-        Post.objects.popular().prefetch_related('author').prefetch_related('tags')[:5]
-            .fetch_with_comments_count()
+        Post.objects.popular().prefetch_related('author')
+                    .prefetch_related('tags')[:5]
+                    .fetch_with_comments_count()
     )
     fresh_posts = (
         all_posts.annotate(comments_count=Count('comments'))
@@ -81,7 +84,9 @@ def index(request):
         'page_posts': [
             serialize_post_optimized(post) for post in most_fresh_posts
         ],
-        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
+        'popular_tags': [
+            serialize_tag_optimized(tag) for tag in most_popular_tags
+        ],
     }
     return render(request, 'index.html', context)
 
@@ -143,7 +148,6 @@ def tag_filter(request, tag_title):
     related_posts = (
         tag.posts.all().annotate(comments_count=Count('comments'))[:20]
     )
-
 
     context = {
         'tag': tag.title,
